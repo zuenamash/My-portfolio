af00fb021a3ad42adda025113f2fe169418e3b8d
zuenamash <mnachariazuenah@gmail.com>
2022-08-16 18:27:00 +0300

    Icons changed

--- index.js
+++ index.js
@@ -14,14 +14,12 @@ $(document).ready(function(){
     });
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
-    // toggle menu/navbar script
+
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
@@ -42,7 +40,6 @@ $(document).ready(function(){
         margin: 20,
         loop: true,
         autoplay: true,
-        // autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
             0:{
